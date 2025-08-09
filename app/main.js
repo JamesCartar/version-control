6bb5ed1de7a0549bd8491bf7a21c6427eacd6ec6
Arjun Yadav <90121395+JamesCartar@users.noreply.github.com>
2025-08-10 00:39:47 +0630

    feat(cat-file): implements cat-file command which go to .git/objects dir, de-compress the file and output it

--- app/main.js
+++ app/main.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.error("Logs from your program will appear here!");
+const GitClient = require("./git/commands/client");
+const { CatFileCommand } = require("./git/commands");
+
+const gitClient = new GitClient();
 
 // Uncomment this block to pass the first stage
 const command = process.argv[2];
@@ -10,6 +12,9 @@ const command = process.argv[2];
 switch (command) {
   case "init":
     createGitDirectory();
+    break;
+  case "cat-file":
+    handleCatFileCommand();
     break;
   default:
     throw new Error(`Unknown command ${command}`);
@@ -27,4 +32,12 @@ function createGitDirectory() {
     "ref: refs/heads/main\n"
   );
   console.log("Initialized git directory");
+}
+
+function handleCatFileCommand() {
+  const flag = process.argv[3];
+  const commitSHA = process.argv[4];
+
+  const command = new CatFileCommand(flag, commitSHA);
+  gitClient.run(command);
 }
